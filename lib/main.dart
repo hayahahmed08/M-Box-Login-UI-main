8a8ec2df43f89f64d9332dbb4954710d4225c74d
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-12 02:38:50 +0500

    Updated login UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,28 +10,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(
           child: Column(
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 50,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image(
+                  const Image(
                     height: 50,
                     width: 50,
                     image: AssetImage('images/logo.png'),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 10,
                   ),
-                  Column(
+                  const Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
@@ -51,13 +52,13 @@ class MyApp extends StatelessWidget {
                         ),
                       ),
                     ],
-                  )
+                  ),
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
-              Center(
+              const Center(
                 child: Text(
                   "Login",
                   style: TextStyle(
@@ -67,10 +68,10 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              Center(
+              const Center(
                 child: Text(
                   "Lorem ipsum dolor sit amet, \n consectetur adipiscing elit",
                   textAlign: TextAlign.center,
@@ -80,6 +81,98 @@ class MyApp extends StatelessWidget {
                     color: Color(0xff4C5980),
                   ),
                 ),
+              ),
+              const SizedBox(height: 20, width: 40) ,
+              Container(
+                height: 50,
+                width: 350,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    fillColor: Colors.black12,
+                    filled: true,
+                    hintText: 'Email',
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.black26,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 50,
+                width: 350,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    fillColor: Colors.black12,
+                    filled: true,
+                    hintText: 'Password',
+                    prefixIcon: Icon(
+                      Icons.lock_open,
+                      color: Colors.black26,
+                    ),
+                    suffixIcon: Icon(Icons.visibility_off),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
+              Container(
+                height: 50,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    "Log In",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account?",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'rubik medium',
+                      color: Color(0xff4C5980),
+                    ),
+                  ),
+                  const SizedBox(width: 5),
+                  const Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'rubik medium',
+                      color: Colors.orange,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
